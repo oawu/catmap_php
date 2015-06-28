307cec01b41f4bc5074bf34a8f5a134911c3f6c6
OA Wu <comdan66@gmail.com>
2015-06-29 00:11:36 +0800

    Temp.

--- application/views/content/site/maps/index/content.js
+++ application/views/content/site/maps/index/content.js
@@ -14,6 +14,7 @@ $(function () {
   var _markers = [];
   var _markerCluster = null;
   var _isGetPictures = false;
+  var _getPicturesTimer = null;
 
   function formatFloat (num, pos) {
     var size = Math.pow (10, pos);
@@ -21,91 +22,90 @@ $(function () {
   }
 
   function getPictures () {
-    if (_isGetPictures)
-      return;
-    
-    _isGetPictures = true;
+    clearTimeout (_getPicturesTimer);
 
-    $loadingData.addClass ('show');
-    var northEast = _map.getBounds().getNorthEast ();
-    var southWest = _map.getBounds().getSouthWest ();
+    _getPicturesTimer = setTimeout (function () {
+          console.error ('-');
+      if (_isGetPictures)
+        return;
+      
+      $loadingData.addClass ('show');
+      _isGetPictures = true;
 
-    $.ajax ({
-      url: $('#get_pictures_url').val (),
-      data: { NorthEast: {latitude: northEast.lat (), longitude: northEast.lng ()},
-              SouthWest: {latitude: southWest.lat (), longitude: southWest.lng ()},  },
-      async: true, cache: false, dataType: 'json', type: 'POST',
-      beforeSend: function () {}
-    })
-    .done (function (result) {
+      var northEast = _map.getBounds().getNorthEast ();
+      var southWest = _map.getBounds().getSouthWest ();
 
-      if (result.status) {
+      $.ajax ({
+        url: $('#get_pictures_url').val (),
+        data: { NorthEast: {latitude: northEast.lat (), longitude: northEast.lng ()},
+                SouthWest: {latitude: southWest.lat (), longitude: southWest.lng ()},  },
+        async: true, cache: false, dataType: 'json', type: 'POST',
+        beforeSend: function () {}
+      })
+      .done (function (result) {
 
-        var markers = result.pictures.map (function (t) {
-          var markerWithLabel = new MarkerWithLabel ({
-            position: new google.maps.LatLng (t.lat, t.lng),
-            draggable: false,
-            raiseOnDrag: false,
-            clickable: true,
-            labelContent: ''+
-              '<div class="img">'+
-                '<img src="' + t.url + '" />'+
-              '</div>'+
-            '',
-            labelAnchor: new google.maps.Point (50, 50),
-            labelClass: "marker_label",
-            icon: {
-              path: 'M 0 0',
-              strokeColor: 'rgba(249, 39, 114, 0)',
-              strokeWeight: 1,
-              fillColor: 'rgba(249, 39, 114, 0)',
-              fillOpacity: 0
-            },
-            initCallback: function (e) { $(e).find ('.img').imgLiquid ({verticalAlign: 'top'}); }
+        if (result.status) {
+
+          var markers = result.pictures.map (function (t) {
+            var markerWithLabel = new MarkerWithLabel ({
+              position: new google.maps.LatLng (t.lat, t.lng),
+              draggable: false,
+              raiseOnDrag: false,
+              clickable: true,
+              labelContent: '<div class="img"><img src="' + t.url + '" /></div>',
+              labelAnchor: new google.maps.Point (50, 50),
+              labelClass: "marker_label",
+              icon: {
+                path: 'M 0 0',
+                strokeColor: 'rgba(249, 39, 114, 0)',
+                strokeWeight: 1,
+                fillColor: 'rgba(249, 39, 114, 0)',
+                fillOpacity: 0
+              },
+              initCallback: function (e) { $(e).find ('.img').imgLiquid ({verticalAlign: 'top'}); }
+            });
+
+            google.maps.event.addListener(markerWithLabel, 'click', function () { });
+
+            return {
+              id: t.id,
+              marker: markerWithLabel
+            };
           });
 
-          google.maps.event.addListener(markerWithLabel, 'click', function () { });
-
-          return {
-            id: t.id,
-            marker: markerWithLabel
-          };
-        });
-
-        if (!_markers.length) {
-          _markers = markers.map (function (t) {
-            _markerCluster.addMarker (t.marker);
-            return t;
-          });
-        } else {
-          var marker_ids = markers.map (function (t) { return t.id; });
-          _markers = _markers.map (function (t) {
-            if ($.inArray (t.id, marker_ids) != -1)
+          if (!_markers.length) {
+            _markers = markers.map (function (t) {
+              _markerCluster.addMarker (t.marker);
               return t;
+            });
+          } else {
+            var marker_ids = markers.map (function (t) { return t.id; });
+            _markers = _markers.map (function (t) {
+              if ($.inArray (t.id, marker_ids) != -1)
+                return t;
 
-            _markerCluster.removeMarker (t.marker);
-            return null;
-          }).filter (function (t) {
-            return t;
-          });
-          marker_ids = _markers.map (function (t) { return t.id; });
-          markers = markers.map (function (t) {
-            if ($.inArray (t.id, marker_ids) != -1)
+              _markerCluster.removeMarker (t.marker);
               return null;
+            }).filter (function (t) { return t; });
 
-            _markerCluster.addMarker (t.marker);
-            return t;
-          }).filter (function (t) {
-            return t;
-          });
-          _markers = _markers.concat (markers);
+            marker_ids = _markers.map (function (t) { return t.id; });
+            
+            markers = markers.map (function (t) {
+              if ($.inArray (t.id, marker_ids) != -1)
+                return null;
+
+              _markerCluster.addMarker (t.marker);
+              return t;
+            }).filter (function (t) { return t; });
+            _markers = _markers.concat (markers);
+          }
+          $loadingData.removeClass ('show');
+          _isGetPictures = false;
         }
-        _isGetPictures = false;
-        $loadingData.removeClass ('show');
-      }
-    })
-    .fail (function (result) { ajaxError (result); })
-    .complete (function (result) {});
+      })
+      .fail (function (result) { ajaxError (result); })
+      .complete (function (result) {});
+    }, 750);
   }
 
   function initialize () {
