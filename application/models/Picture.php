41187d190a2dc7bb86e09b9a6d00feda45614261
OA Wu <comdan66@gmail.com>
2015-06-28 13:16:39 +0800

    Fix bug.

--- application/models/Picture.php
+++ application/models/Picture.php
@@ -51,7 +51,7 @@ class Picture extends OaModel {
   public function update_color () {
     $image_utility = ImageUtility::create (FCPATH . implode('/', $this->name->path ()));
 
-    if (($analysis_datas = $image_utility->getAnalysisDatas (1)) && isset ($analysis_datas[0]['color']) && ($analysis_datas = $analysis_datas[0]['color']) && (isset ($analysis_datas['r']) && isset ($analysis_datas['g']) && isset ($analysis_datas['b']))) {
+    if (($analysis_datas = $image_utility->resize (10, 10, 'w')->getAnalysisDatas (1)) && isset ($analysis_datas[0]['color']) && ($analysis_datas = $analysis_datas[0]['color']) && (isset ($analysis_datas['r']) && isset ($analysis_datas['g']) && isset ($analysis_datas['b']))) {
       $this->color_red   = $analysis_datas['r'];
       $this->color_green = $analysis_datas['g'];
       $this->color_blue  = $analysis_datas['b'];
