0668d6e670572bd97b4171d08a7fbf9935e98169
OA Wu <comdan66@gmail.com>
2015-07-05 16:21:41 +0800

    temp.

--- application/third_party/orm_uploaders/UserAvatarImageUploader.php
+++ application/third_party/orm_uploaders/UserAvatarImageUploader.php
@@ -10,9 +10,9 @@ class UserAvatarImageUploader extends OrmImageUploader {
   public function getVersions () {
     return array (
         '' => array (),
-        '65x65c' => array ('adaptiveResizeQuadrant', 65, 65, 't'),
-        '100x100c' => array ('adaptiveResizeQuadrant', 100, 100, 't'),
-        '200x200c' => array ('adaptiveResizeQuadrant', 200, 200, 't')
+        '65x65t' => array ('adaptiveResizeQuadrant', 65, 65, 't'),
+        '100x100t' => array ('adaptiveResizeQuadrant', 100, 100, 't'),
+        '200x200t' => array ('adaptiveResizeQuadrant', 200, 200, 't')
         // '140x140c' => array ('adaptiveResizeQuadrant', 140, 140, 't')
       );
   }
